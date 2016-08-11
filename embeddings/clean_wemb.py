cd22d61f088691fba4f944340cf90f97de79d84c
Jan Deriu <jan.deriu@gmail.com>
2016-08-11 14:08:00 +0200

    removed files

--- embeddings/clean_wemb.py
+++ embeddings/clean_wemb.py
@@ -1,8 +1,12 @@
-
+from tqdm import tqdm
 fname = 'smiley_tweets_embedding_multilingual300M'
 out_fname = 'smiley_tweets_embedding_multilingual300M_clean'
 
 ofile = open(out_fname,'w')
-for line in open(fname,'r'):
-    if len(line.split('\t')) == 53:
+ifile = open(fname,'r')
+header = ifile.readline()
+ofile.write(header)
+for line in tqdm(ifile):
+    l = len(line.split(' '))
+    if l == 53:
         ofile.write(line)
